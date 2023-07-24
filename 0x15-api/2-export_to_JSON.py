f686cabee22d36b42e8233046d88528f7cc010f0
Rediet <redietkfle@gmail.com>
2023-07-24 03:31:02 -0700

    Export to json file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,32 +7,33 @@ import json
 import requests
 import sys
 
-employee_id = sys.argv[1]
-""" Collects emplyee ID. """
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    """ Collects emplyee ID. """
 
-response = requests.get(f"https://jsonplaceholder"
-                        f".typicode.com/todos?userId={employee_id}")
-todos = response.json()
-"""
-Retrieve employee TODO list via
-request to API and parse response.
-"""
+    response = requests.get(f"https://jsonplaceholder"
+                            f".typicode.com/todos?userId={employee_id}")
+    todos = response.json()
+    """
+    Retrieve employee TODO list via
+    request to API and parse response.
+    """
 
-response = requests.get(f"https://jsonplaceholder"
-                        f".typicode.com/users/{employee_id}")
-employee_name = response.json()["username"]
+    response = requests.get(f"https://jsonplaceholder"
+                            f".typicode.com/users/{employee_id}")
+    employee_name = response.json()["username"]
 
-filename = (f"{employee_id}.json")
-todo_progress = {employee_id: []}
-""" Dictionary stores to-do list progress. """
+    filename = (f"{employee_id}.json")
+    todo_progress = {employee_id: []}
+    """ Dictionary stores to-do list progress. """
 
-for todo in todos:
-    task = {
-            "task": todo["title"],
-            "completed": todo["completed"],
-            "username": employee_name
-    }
-    todo_progress[employee_id].append(task)
+    for todo in todos:
+        task = {
+                "task": todo["title"],
+                "completed": todo["completed"],
+                "username": employee_name
+        }
+        todo_progress[employee_id].append(task)
 
-with open(filename, "w") as json_file:
-    json.dump(todo_progress, json_file)
+    with open(filename, "w") as json_file:
+        json.dump(todo_progress, json_file)
