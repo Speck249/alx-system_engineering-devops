0d4f989617b7775622d7ad95540c90cacae4df3c
Rediet <redietkfle@gmail.com>
2023-07-24 02:39:55 -0700

    Display todo list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,40 @@
 #!/usr/bin/python3
 """
-Given employee ID, Module returns
-information about TODO list progress.
+Module returns information
+about TODO list progress.
 """
 import requests
 import sys
 
-employee_id = sys.argv[1]
-""" Collects emplyee ID. """
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    """ Collects emplyee ID. """
 
-response = requests.get(f"https://jsonplaceholder"
-                        f".typicode.com/todos?userId={employee_id}")
-todos = response.json()
-"""
-Retrieve employee TODO list via
-request to API and parse response.
-"""
+    response = requests.get(f"https://jsonplaceholder"
+                            f".typicode.com/todos?userId={employee_id}")
+    todos = response.json()
+    """
+    Retrieve employee TODO list via
+    request to API and parse response.
+    """
 
-total_completed_tasks = 0
-total_tasks = len(todos)
-completed_tasks_titles = []
-for todo in todos:
-    if todo["completed"]:
-        total_completed_tasks += 1
-        completed_tasks_titles.append(todo["title"])
+    total_completed_tasks = 0
+    total_tasks = len(todos)
+    completed_tasks_titles = []
 
-response = requests.get(f"https://jsonplaceholder"
-                        f".typicode.com/users/{employee_id}")
-employee_name = response.json()["name"]
-""" Retrieve employee name. """
+    for todo in todos:
+        if todo["completed"]:
+            total_completed_tasks += 1
+            completed_tasks_titles.append(todo["title"])
 
-print(f"Employee {employee_name} is done"
-      f" with tasks({total_completed_tasks}/{total_tasks}):")
-for title in completed_tasks_titles:
-    print(f"\t {title}")
+    response = requests.get(f"https://jsonplaceholder"
+                            f".typicode.com/users/{employee_id}")
+
+    employee_name = response.json()["name"]
+    """ Retrieve employee name. """
+
+    print(f"Employee {employee_name} is done"
+          f" with tasks({total_completed_tasks}/{total_tasks}):")
+
+    for title in completed_tasks_titles:
+        print(f"\t {title}")
