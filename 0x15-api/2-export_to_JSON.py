1252a4a4356c1e0c56aac57bbdeb483028864687
Rediet <redietkfle@gmail.com>
2023-07-24 02:06:52 -0700

    Export to json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module exports to-do list
+progress to json file.
+"""
+import json
+import requests
+import sys
+
+employee_id = sys.argv[1]
+""" Collects emplyee ID. """
+
+response = requests.get(f"https://jsonplaceholder"
+                        f".typicode.com/todos?userId={employee_id}")
+todos = response.json()
+"""
+Retrieve employee TODO list via
+request to API and parse response.
+"""
+
+response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+employee_name = response.json()["username"]
+
+filename = (f"{employee_id}.json")
+todo_progress = {employee_id: []}
+""" Dictionary stores to-do list progress. """
+
+for todo in todos:
+    task = {
+            "task": todo["title"],
+            "completed": todo["completed"],
+            "username": employee_name
+    }
+    todo_progress[employee_id].append(task)
+
+with open(filename, "w") as json_file:
+    json.dump(todo_progress, json_file)
