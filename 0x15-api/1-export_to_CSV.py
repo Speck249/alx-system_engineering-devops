1dbfec04c4f0b30065fad6625e2cf23d842c2f85
Rediet <redietkfle@gmail.com>
2023-07-24 02:16:10 -0700

    Export to csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -18,7 +18,8 @@ Retrieve employee TODO list via
 request to API and parse response.
 """
 
-response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+response = requests.get(f"https://jsonplaceholder"
+                        f".typicode.com/users/{employee_id}")
 employee_name = response.json()["username"]
 
 filename = (f"{employee_id}.csv")
@@ -27,11 +28,14 @@ filename = (f"{employee_id}.csv")
 with open(filename, 'w', newline="") as csv_file:
     """ Writes employee TOD list progress to a csv file. """
     writer = csv.writer(csv_file)
-    writer.writerow(["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"])
+    writer.writerow(["USER_ID", "USERNAME",
+                     "TASK_COMPLETED_STATUS", "TASK_TITLE"])
     for todo in todos:
         if not todo["completed"]:
-            writer.writerow([employee_id, employee_name, "False", todo["title"]])
+            writer.writerow([employee_id, employee_name,
+                            "False", todo["title"]])
 
     for todo in todos:
         if todo["completed"]:
-            writer.writerow([employee_id, employee_name, "True", todo["title"]])
+            writer.writerow([employee_id, employee_name,
+                            "True", todo["title"]])
