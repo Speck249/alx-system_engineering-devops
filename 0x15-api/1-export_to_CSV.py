670aa795231395d72c21643d27789027401b833c
Rediet <redietkfle@gmail.com>
2023-07-24 02:42:45 -0700

    Export to csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,35 +7,37 @@ import csv
 import requests
 import sys
 
-employee_id = sys.argv[1]
-""" Collects emplyee ID. """
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    """ Collects emplyee ID. """
 
-response = requests.get(f"https://jsonplaceholder"
-                        f".typicode.com/todos?userId={employee_id}")
-todos = response.json()
-"""
-Retrieve employee TODO list via
-request to API and parse response.
-"""
+    response = requests.get(f"https://jsonplaceholder"
+                            f".typicode.com/todos?userId={employee_id}")
+    todos = response.json()
+    """
+    Retrieve employee TODO list via
+    request to API and parse response.
+    """
 
-response = requests.get(f"https://jsonplaceholder"
-                        f".typicode.com/users/{employee_id}")
-employee_name = response.json()["username"]
+    response = requests.get(f"https://jsonplaceholder"
+                            f".typicode.com/users/{employee_id}")
+    employee_name = response.json()["username"]
 
-filename = (f"{employee_id}.csv")
-""" Retrieve employee name and define csv file. """
+    filename = (f"{employee_id}.csv")
+    """ Retrieve employee name and define csv file. """
 
-with open(filename, 'w', newline="") as csv_file:
-    """ Writes employee TOD list progress to a csv file. """
-    writer = csv.writer(csv_file)
-    writer.writerow(["USER_ID", "USERNAME",
-                     "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-    for todo in todos:
-        if not todo["completed"]:
-            writer.writerow([employee_id, employee_name,
-                            "False", todo["title"]])
+    with open(filename, 'w', newline="") as csv_file:
+        """ Writes employee TOD list progress to a csv file. """
+        writer = csv.writer(csv_file)
+        writer.writerow(["USER_ID", "USERNAME",
+                        "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-    for todo in todos:
-        if todo["completed"]:
-            writer.writerow([employee_id, employee_name,
-                            "True", todo["title"]])
+        for todo in todos:
+            if not todo["completed"]:
+                writer.writerow([employee_id, employee_name,
+                                "False", todo["title"]])
+
+        for todo in todos:
+            if todo["completed"]:
+                writer.writerow([employee_id, employee_name,
+                                "True", todo["title"]])
