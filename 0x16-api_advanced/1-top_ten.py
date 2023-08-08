936768471dd5e2d733b92ff152676cfcff982e83
Rediet <redietkfle@gmail.com>
2023-08-08 10:36:19 +0000

    Return top posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,9 +14,11 @@ def top_ten(subreddit):
     headers = {
         'User-Agent', 'Customer User Agent'
     }
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
+    url = 'https://www.reddit.com/r/{}/' \
+          'hot.json?limit=10'.format(subreddit)
+    response = requests.get(url, headers=headers,
+                            allow_redirects=False)
+
     if reponse.status_code == 200:
         data = response.json()
         posts = data['data']['childrens']
