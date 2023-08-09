8efb34a30d2700e5a229635ad2242b17e4ff406f
Rediet <redietkfle@gmail.com>
2023-08-09 16:21:52 +0000

    Resolve 500 error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+$target_file = '/var/www/html/wp-settings.php'
+
+exec { 'replace_line':
+   command => "sed -i 's/phpp/php/g' ${target_file}",
+   path    => ['bin', 'usr/bin'],
+   onlyif  => "grep -q 'phpp' ${target_file}",
+   require => File[target_file],
+}
