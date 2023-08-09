6d8f66c3132ce9ba039198743e918305d31261ed
Rediet <redietkfle@gmail.com>
2023-08-09 16:52:03 +0000

    Resolve 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,9 @@
+# Puppet manifest resolves 500 error
 $target_file = '/var/www/html/wp-settings.php'
 
 exec { 'replace_line':
-   command => "sed -i 's/phpp/php/g' ${target_file}",
-   path    => ['bin', 'usr/bin'],
-   onlyif  => "grep -q 'phpp' ${target_file}",
-   require => File[target_file],
+  command => "sed -i 's/phpp/php/g' ${target_file}",
+  path    => ['bin', 'usr/bin'],
+  onlyif  => "grep -q 'phpp' ${target_file}",
+  require => File[target_file],
 }
