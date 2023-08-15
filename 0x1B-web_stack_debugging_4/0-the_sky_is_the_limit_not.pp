981929efc2d54bfadcb025a6eaf2ee7703d9d6d4
Rediet <redietkfle@gmail.com>
2023-08-15 09:55:59 +0000

    Fix failed HTTP requests

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+file { '/etc/default/nginx':
+  ensure  => file,
+  content => "ULIMIT=\"-n 4096\"\n",
+  notify  => Exec['nginx-restart'],
+}
+
+exec { 'nginx-restart':
+  command     => '/etc/init.d/nginx restart',
+  refreshonly => true,
+}
