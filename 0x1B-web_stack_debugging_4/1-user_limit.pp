8a724c27a9ffaf678d18551ccd16189d94a8e18e
Rediet <redietkfle@gmail.com>
2023-08-15 11:25:11 +0000

    Login with holberton user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+exec { 'replace_nofile_limit_1':
+  command => '/usr/bin/sudo /bin/sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before  => Exec['replace_nofile_limit_2'],
+}
+
+exec { 'replace_nofile_limit_2':
+  command => '/usr/bin/sudo /bin/sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+}
