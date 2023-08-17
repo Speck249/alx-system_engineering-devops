9cd14ba8d8129133592528c1151ee67021059e4f
Rediet <redietkfle@gmail.com>
2023-08-17 06:16:40 +0000

    Fix failed HTTP requests

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,8 @@
+# Puppet manifest fixes failed HTTP requests
 file { '/etc/default/nginx':
-  ensure   => file,
-  content  => "ULIMIT=\"-n 4096\"\n",
-  notify   => Exec['nginx-restart'],
+  ensure  => file,
+  content => "ULIMIT=\"-n 4096\"\n",
+  notify  => Exec['nginx-restart'],
 }
 
 exec { 'nginx-restart':
