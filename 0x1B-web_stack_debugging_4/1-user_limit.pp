309463e4edc9fd044bcf026151a1c30ffc238cff
Rediet <redietkfle@gmail.com>
2023-08-17 06:19:23 +0000

    Login with holberton

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,4 @@
+# Puppet manifest logins in with holberton user
 exec { 'replace_nofile_limit_1':
   command => '/usr/bin/sudo /bin/sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
   before  => Exec['replace_nofile_limit_2'],
