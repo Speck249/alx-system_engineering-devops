513886d5cea62a31d6f9dfa8e9b3b21d8b6fca7b
rediet <redietkfle@gmail.com>
2024-05-29 06:50:12 -0700

    Save complete employee tasks to JSON file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,39 +1,46 @@
 #!/usr/bin/python3
 """
-Module exports to-do list
-progress to json file.
+Module saves API response to JSON file.
 """
+import csv
 import json
 import requests
 import sys
 
+
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-    """ Collects emplyee ID. """
-
-    response = requests.get(f"https://jsonplaceholder"
-                            f".typicode.com/todos?userId={employee_id}")
-    todos = response.json()
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    response = requests.get(url)
+    employee_info = response.json()
     """
-    Retrieve employee TODO list via
-    request to API and parse response.
+    Retrieve complete information about employee with
+    target ID in JSON format.
     """
 
-    response = requests.get(f"https://jsonplaceholder"
-                            f".typicode.com/users/{employee_id}")
-    employee_name = response.json()["username"]
+    url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    response = requests.get(url)
+    employee_todo_list = response.json()
+    """
+    Retrieve employee's todo list with the
+    given ID.
+    """
 
-    filename = (f"{employee_id}.json")
-    todo_progress = {employee_id: []}
-    """ Dictionary stores to-do list progress. """
+row_data = {employee_id: []}
+for item in employee_todo_list:
+    """
+    Format each row with target information about
+    employee's todo list along with name.
+    """
+    row = {
+        'task': item['title'],
+        'completed': item['completed'],
+        'username': employee_info['username']
+    }
+    row_data[employee_id].append(row)
 
-    for todo in todos:
-        task = {
-                "task": todo["title"],
-                "completed": todo["completed"],
-                "username": employee_name
-        }
-        todo_progress[employee_id].append(task)
-
-    with open(filename, "w") as json_file:
-        json.dump(todo_progress, json_file)
+with open('USER_ID.json', 'w') as File:
+    """
+    Save into JSON file serialized employee data.
+    """
+    json.dump(row_data, File)
